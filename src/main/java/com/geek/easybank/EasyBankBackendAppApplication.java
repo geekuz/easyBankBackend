2f195454a240eda3bace59654f1061ac975aa02b
geekuz <otabekgeek@gmail.com>
2024-08-26 16:23:19 +0500

    app creation

---
+++ src/main/java/com/geek/easybank/EasyBankBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.geek.easybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBankBackendAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBankBackendAppApplication.class, args);
+    }
+
+}
