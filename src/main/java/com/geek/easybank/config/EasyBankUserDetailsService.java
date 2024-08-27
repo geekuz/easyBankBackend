32aeb569f1fbad6d1b497e2d86509c9ce24c566f
creativeuz <creative.otabek@gmail.com>
2024-08-27 22:14:11 +0500

    create user

--- src/main/java/com/geek/easybank/config/EasyBankUserDetailsService.java
+++ src/main/java/com/geek/easybank/config/EasyBankUserDetailsService.java
@@ -3,20 +3,27 @@ package com.geek.easybank.config;
 import com.geek.easybank.model.Customer;
 import com.geek.easybank.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EasyBankUserDetailsService implements UserDetailsService {
+
     private final CustomerRepository customerRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Customer> byEmail = customerRepository.findByEmail(username).orElseThrow();
+        Customer customer = customerRepository.findByEmail(username)
+            .orElseThrow(() -> new UsernameNotFoundException("username not found"));
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
     }
 }
